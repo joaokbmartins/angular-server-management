8e5f33c617ef18835ac118d1b10d45f03fd9dd33
João Martins <martins.joao1909@gmail.com>
2020-11-27 19:02:24 -0300

    App structure created

---
+++ src/app/components/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html'
+})
+export class EditServerComponent {
+
+}
